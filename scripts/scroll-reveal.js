0290c42fa6ad78f2196e2b8df939d188f21f536d
AndrewMoreira91 <andrewsantos9113@gmail.com>
2023-09-15 23:13:29 -0300

    Mudanças na Pagina Inicial

---
+++ scripts/scroll-reveal.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('#resumo1',{
+    duration: 2000,
+    rotate: {x: 0,  y:80, z:0}
+});
+
+sr.reveal('#resumo2',{
+    duration: 2000,
+    rotate: {x: 0,  y:80, z:0}
+});
+
+sr.reveal('#resumo3',{
+    duration: 2000,
+    rotate: {x: 0,  y:80, z:0}
+});
+
+sr.reveal('#resumos',{
+    duration: 2000,
+    rotate: {x: 0,  y:80, z:0}
+});
+
+
+sr.reveal('#quem-somos',{
+    duration: 2000,
+    rotate: {x: 80,  y:0, z:0}
+});
+
+sr.reveal('#experimente',{
+    duration: 2000,
+    rotate: {x: 80,  y:0, z:0}
+});
